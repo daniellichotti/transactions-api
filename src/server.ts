8ba4aca17dc04bb888e15b5975fc71827030724c
Daniel Lichotti <danielclichotti@gmail.com>
2024-12-30 18:17:59 -0300

    Alterando scripts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import { app } from "./app"
-import { env } from "./env"
+import { app } from "./app";
+import { env } from "./env";
+
+// Porta e host
+const PORT = env.PORT || 3000; // Porta do Render ou 3000 como fallback
+const HOST = '0.0.0.0'; // Necessário para ambientes como Render
 
 app.listen({
-  port: env.PORT,
+  port: PORT,
+  host: HOST
 }).then(() => {
   console.log(`Server listening at http://localhost:${env.PORT}`)
 })
